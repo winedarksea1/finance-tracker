db52da467cbbceb5f82687a400de3ce82ccd2cac
Ubuntu <ubuntu@ip-172-31-27-184.us-east-2.compute.internal>
2020-02-19 17:43:12 +0000

    added search error handling in UI

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,7 +7,11 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )      
     
-    return new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    begin
+      return new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
   end
   
 end
